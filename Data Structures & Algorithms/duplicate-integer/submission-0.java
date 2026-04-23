cf90294e2d34c0a2f360e3f0bf7786cbb1adfa4e
3iswajit <66627619+3iswajit@users.noreply.github.com>
2026-04-23 17:31:54 +0530

    Remove: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean hasDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int i : nums){
-            set.add(i);
-        }
-        return (nums.length!=set.size())?true:false;
-    }
-}
